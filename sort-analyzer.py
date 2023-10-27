2207fdcdacb54cdf531372b91c3e781f33f3e5d7
Teehxw <112659668+Teehxw@users.noreply.github.com>
2023-10-27 13:30:04 -0600

    Update sort-analyzer.py

--- sort-analyzer.py
+++ sort-analyzer.py
@@ -24,14 +24,45 @@ nearlySortedData = loadDataArray("data-files/nearly-sorted-values.txt")
 fewUniqueData = loadDataArray("data-files/few-unique-values.txt")
 
 # VERIFY LOADED DATA BY PRINTING FIRST 50 ELEMENTS
+
+def bubbleSort(anArray):
+  for numCompare in range(len(anArray)-1,0, -1):
+    for x in range(numCompare):
+      if anArray[x] > anArray[x + 1]:
+        anArray[x], anArray[x + 1] = anArray[x + 1], anArray[x]
+  print(anArray)
+
+def selectionSort(anArray):
+  for fillSlot in range(len(anArray)):
+    minIndex = fillSlot
+    for x in range(fillSlot, len(anArray)):
+      if anArray[x] < anArray[minIndex]:
+        minIndex = x
+    anArray[fillSlot], anArray[minIndex] = anArray[minIndex], anArray[fillSlot]
+
+  print(anArray)
+
+def insertionSort(anArray):
+  for i in range(1, len(anArray)):
+    insertVal = anArray[i]
+    insertPos = i
+    while insertPos > 0 and anArray[insertPos - 1] > insertVal:
+      anArray[insertPos] = anArray[insertPos - 1]
+      insertPos = insertPos - 1
+      anArray[insertPos] = insertVal
+  print(anArray)
+
+
+"""
 print(randomData[0:50])
 print(reversedData[0:50])
 print(nearlySortedData[0:50])
 print(fewUniqueData[0:50])
-
+"""
 
 # EXAMPLE OF HOW TO TIME DURATION OF A SORT ALGORITHM
-# startTime = time.time()
-# bubbleSort(randomData)
-# endTime = time.time()
-# print(f"Bubble Sort Random Data: {endTime - startTime} seconds")
+startTime = time.time()
+insertionSort(randomData)
+
+endTime = time.time()
+print(f"Insertion Sort Random Data: {endTime - startTime} seconds")
